a6e51b2a258c54a1159a79464fdc3e97afed041a
Jerry Eshbaugh <Jerry.Eshbaugh@ocio.usda.gov>
2019-09-29 15:28:16 -0600

    Creating an mp3 file now.

--- wav2mp3.py
+++ wav2mp3.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import glob
-from pydub import AudioSegment
+#from pydub import AudioSegment
+import pydub
 
-filePath = "/media/CR/2019/Sept27/927/"
+filePath = '/media/CR/2019/Test/'
+filePath = '/media/CR/2019/Sept27/927/'
 
-wavFilePath = filePath + "*.wav"
+wavFilePath = filePath + '*'
 files = glob.glob( wavFilePath )
+print( 'Processing files:', files )
 
 for in_file in files:
-    out_file = in_file.replace( ".wav", ".mp3" )
-    print( in_file, out_file )
+    if not in_file.endswith( '.wav') :
+        continue
 
+    out_file = in_file.replace( '.wav', '.mp3' )
+    print( 'Using file:', in_file, '  to create file:', out_file )
 
-# convert wav to mp3                                                            
-sound = AudioSegment.from_wav(in_file)
-sound.export(out_file, format="mp3")
+    # convert wav to mp3                                                            
+    sound = pydub.AudioSegment.from_wav(in_file)
+    sound.export(out_file, format='mp3')
